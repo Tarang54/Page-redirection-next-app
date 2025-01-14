d4ae4d50c555334abb14119dc97d8a7561e62b08
Tarang <tarang.sultania@classplus.co>
2025-01-14 13:17:22 +0530

    added styles

--- src/app/prediction/[name]/page.tsx
+++ src/app/prediction/[name]/page.tsx
@@ -2,7 +2,7 @@ interface Params{
   params:{ name : string };
 }
 
-export default function Page({params} : Params) {
+export default function Page({params} : Params ) {
   return (
     <div className="flex justify-center items-center min-h-screen font-bold text-[50px]">
         {""}
